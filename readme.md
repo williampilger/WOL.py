fb5b2ec0020bff346ca702fbcd00619b593901e9
williamauthenty <pilger.will@gmail.com>
2021-09-18 11:15:19 -0300

    Upload

---
+++ readme.md
@@ -0,0 +1,43 @@
+# WakeUp-On-Lan Python3 Script
+
+Este script funciona em Windows e Linux.
+Pensado para iniciar computadores na rede **local** utilizando Magic Packet.
+
+## Utilização
+
+Faça o downloas do repositório, e configure seus hosts no arquivo `wol_config.ini`, conforme o exemplo abaixo:
+
+```
+host1	192.168.2.255	a4:5e:81:f8:a9:9a
+host2	192.168.2.255	7a:d8:8c:8b:c9:74
+```
+
+obs: Utilize uma linha por host, e espaços de TAB para separar as configurações.
+
+**Executar - Windows**
+
+Abra o terminal no diretório onde o arquivo está e digite:
+
+> wol.py host1
+
+**Executar - Linux**
+
+Abra o terminal no diretório onde o arquivo está e digite:
+
+> python3 wol.py host1
+
+**Help**
+
+Para obter ajuda use:
+
+> wol.py -help
+
+## Sobre
+
+William Pilger | Authenty AE | Bom Principio - RS - Brasil
+
+**Algumas Referências**
+
+Aqui estão listadas algumas referências, nas quais foram realizadas algumas consultas.
+
+	- [Wake-On-Lan-Python](https://github.com/bentasker/Wake-On-Lan-Python)
---
+++ wol.py
@@ -0,0 +1,77 @@
+try:
+    import socket
+    import struct
+    import os
+    import sys
+    import multiplat as mp
+    import backend as be
+except:
+    print("Biblioteca necessária não disponível.\n\nEstamos finalizando.")
+    quit()
+
+
+def WakeupOnLan(broadcast_ip, host_mac):
+    mac_adrs = host_mac.replace(host_mac[2], '')#remover separadores
+    data = ''.join(['FFFFFFFFFFFF', mac_adrs * 20])
+    send_data = b''
+    for j in range(0, len(data), 2):
+        send_data = b''.join([
+            send_data,
+            struct.pack('B', int(data[j: j + 2], 16))
+        ])
+    # Broadcast it to the LAN.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(send_data, (broadcast_ip, 7))
+    print(f'Magic Packet enviado para {host_mac} ({broadcast_ip})')
+    return True
+
+# Função para ler dados de arquivo
+# PARAMS: filename(String) -> Nome do arquivo que possui as configurações
+# RETURN: data(dictionary) -> Ex. {'hostName' : ['192.168.2.255','a4:5e:81:f8:a9:9a']}
+def ler_config(fileName):
+    if not os.path.exists(fileName):
+        os.system("echo hostName\t192.168.2.255\ta4:5e:81:f8:a9:9a > wol_config.ini")
+        return False
+    data = {}
+    with open(fileName, "r") as arquivo:
+        for linha in arquivo:
+            linha = linha.replace('\n', '')#tirar fim de linha
+            host = linha.split('\t')
+            if(len(host) == 3):
+                data[host[0]] = host[1:]
+    return data
+
+if __name__ == '__main__':
+    config = ler_config('wol_config.ini')
+    if(config):
+        prompt = ("-p" in sys.argv) #Obtém parametros informados
+        if ((arg := sys.argv[-1]) == '-all'):
+            for host in config:
+                WakeupOnLan(config[host][0],config[host][1])
+        elif(arg == '-list'):
+            for host in config:
+                print(f"Host: {host} [ {config[host][0]} | {config[host][1]} ]")
+        elif(arg == '-help'):
+            print(" Desenvolvido por William Pilger | AuthentyAE | Bom Princípio - RS - Brasil")
+            print("      wol.py [ARG]")
+            print("           ARG:")
+            print("              -all - Envia o Magic Packet para todos os Hosts configurados.")
+            print("              -list - Lista todos os Hosts configurados.")
+            print("              -help - Envia o Magic Packet para todos os Hosts configurados.")
+            print("              seuHost - Envia o Magic Packet para o host informado, se ele existir")
+            print("\n      wol_config.ini")
+            print("              Este arquivo DEVE conter a configuração dos hosts, e deve estar neste diretório.")
+            print("              A correta disposição dos dados é:")
+            print("                     hostname    broadcast_ip    mac_adrs")
+            print("                     exemplo     192.168.2.255   a4:5e:81:f8:a9:9a")
+            print("              A separação dos dados é feita por TAB.")
+            print("              Caso o arquivo não exista, um exemplo será criado ao executar o WOL.")
+        else:
+            if(arg in config):
+                WakeupOnLan(config[arg][0], config[arg][1])
+            else:
+                print("Host não existe.")
+                print("use 'wol.py -help' para obter ajuda.")
+    else:
+        be.sair(2)
